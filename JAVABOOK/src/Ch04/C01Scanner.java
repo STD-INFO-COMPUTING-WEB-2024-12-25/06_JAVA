a9699d94da549c74fad0becb188a1d9a2469f11c
junwoogyun <jwg8910@naver.com>
2025-02-26 14:47:51 +0900

    03 DAY

--- JAVABOOK/src/Ch04/C01Scanner.java
+++ JAVABOOK/src/Ch04/C01Scanner.java
@@ -10,11 +10,17 @@ public class C01Scanner {
 		// 자동 formatting : ctrl + shifg + f
 		
 		//스트림 
-		//표준입력 스트림
-		//표준출력 스트림
+		//System.in :  표준입력 스트림 생성(요청)
+		//System.out : 표준출력 스트림 생성(요청)
+		//
 		//코드 해석
+		//new Scanner(System.in) : 표준입력 스트림을 연결한 Scanner객체 생성
+		//Scanner sc  : 생성된 Scanner객체의 위치정보(메모리주소)를 저장한 참조변수 sc
+		//Scanner sc = new Scanner(System.in);
+		//- 표준입력스트림에 연결된 Scanner 객체를 생성 이후 
+		//  위치정보를 main Stack영역의 Scanner sc 참조변수에 초기화
 		Scanner sc = new Scanner(System.in);
-
+	
 		System.out.printf("num1 입력 : ");
 		int num1 = sc.nextInt(); // 엔터(\n),스페이스바
 		System.out.printf("num2 입력 : ");
