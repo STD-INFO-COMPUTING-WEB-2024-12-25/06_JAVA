b7924c673803cef172da1853324789451e1eff7b
junwoogyun <jwg8910@naver.com>
2025-02-27 15:49:12 +0900

    04DAY

---
+++ JAVABOOK/src/Ch07/C01While.java
@@ -0,0 +1,106 @@
+package Ch07;
+
+import java.util.Scanner;
+
+public class C01While {
+
+	public static void main(String[] args) {
+		
+		// 10번반복 - "HELLO WORLD"
+		// 탈출용 변수 : i=0
+		// 탈출용 조건식 : i<10
+		// 탈출용 연산식 : i++
+//		while(조건식) {
+//			조건식이 true 인동안 실행되는 종속문장
+//		}
+		
+		//01 기본 - HELLO WORLD 10회 반복
+//		int i=0;
+//		while(i<10) {
+//			System.out.println("HELLO WORLD");
+//			i++;
+//		}
+		
+		//02 기본 - HELLO WORLD N회 반복
+//		Scanner sc = new Scanner(System.in);
+//		int n = sc.nextInt();
+//		int i=0;
+//		while(i<n) {
+//			System.out.println("HELLO WORLD");
+//			i++;
+//		}
+		
+		//03 기본 - 1부터 10까지의 합
+		
+//		int i = 1;		//탈출용 변수
+//		int sum = 0;	//누적합 변수
+//		while(i<=10)	//탈출용 조건식 
+//		{
+//			System.out.println("i : " + i);
+//			sum = sum + i;
+//			i++;		//탈출용 연산식
+//		}
+//		System.out.println("SUM : " + sum);
+		
+		//04 기본 - 1부터 N까지 합
+		
+//		Scanner sc  = new Scanner(System.in);
+//		int i = 1;		//탈출용 변수
+//		int sum = 0;	//누적합 변수
+//		int n = sc.nextInt();
+//		while(i<=n)	//탈출용 조건식 
+//		{
+//			System.out.println("i : " + i);
+//			sum = sum + i;
+//			i++;		//탈출용 연산식
+//		}
+//		System.out.println("SUM : " + sum);	
+		
+		
+		//05 기본 - N부터 M까지 합(N<M) - N>=M 인경우 N과 M을 Swap하고 진행합니다
+		
+//		Scanner sc  = new Scanner(System.in);
+//
+//		int n = sc.nextInt();	//시작값
+//		int m = sc.nextInt();	//종료값
+//		
+//		if(n>=m) {
+//			//잘못 입력한경우(시작값(n)이 끝값(m)보다 큰경우 , (5,3)...)
+//			int tmp = n;
+//			n = m;
+//			m = tmp;
+//		}
+//		
+//		int i = n;		//탈출용 변수
+//		int sum = 0;	//누적합 변수
+//		while(i<=m)	//탈출용 조건식 
+//		{
+//			System.out.println("i : " + i);
+//			sum += i;		//sum = sum + i; 
+//			i++;		//탈출용 연산식
+//		}
+//		System.out.println("SUM : " + sum);	
+		
+		
+		//06 N - M까지 수중(N<M) 짝수의합, 홀수의 합을 각각 구해서 출력하세요
+		//예시)
+		//n= 3(키보드로 입력된 값)
+		//m = 7(키보드로 입력된 값)
+		//짝수의 합 : 10
+		//홀수의 합 : 15
+		
+		//07 1 - N 까지 수중에 4의 배수를 출력하고 4의배수가 아닌 나머지의 합을 구하세요
+		//08 구구단 N단을 출력합니다(2<=N<=9)
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
+
