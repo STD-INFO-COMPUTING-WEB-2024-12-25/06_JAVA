ed69c6b50380f5b34fc016508cfac4a0e5e2c84d
junwoogyun <jwg8910@naver.com>
2025-02-27 11:17:21 +0900

    04 DAY

--- JAVABOOK/src/Ch06/C01IF.java
+++ JAVABOOK/src/Ch06/C01IF.java
@@ -43,36 +43,77 @@ public class C01IF {
 //		
 //		sc.close();
 		
-
-		//문제
+		//-----------------------
+		//1 문제
+		//-----------------------
 		// 정수 한개값을 입력받아 3의 배수이면 해당 수를 출력하세요
 		// 3의 배수이면서 5의 배수라면 출력 - 
 		// 순서도도 그려보세요 - 
 		
-		Scanner sc = new Scanner(System.in);
-		System.out.print("정수입력 : " );
-		int num = sc.nextInt();
+//		Scanner sc = new Scanner(System.in);
+//		System.out.print("정수입력 : " );
+//		int num = sc.nextInt();
+//		
+//		if(num%3==0) {
+//			System.out.printf("%d 는 3의 배수입니다\n",num);
+//			
+//			if(num%5==0) {
+//				System.out.printf("%d 는 5의 배수입니다\n",num);
+//			}
+//		}
+//		
+//		sc.close();
 		
-		if(num%3==0) {
-			System.out.printf("%d 는 3의 배수입니다\n",num);
-			
-			if(num%5==0) {
-				System.out.printf("%d 는 5의 배수입니다\n",num);
-			}
-		}
 		
-		sc.close();
-		
-		//문제
+		//-----------------------
+		//2 문제
+		//-----------------------
 		// 두개의 정수를 입력받아 큰 수 출력
 		
-		//문제(☆)
+//		Scanner sc = new Scanner(System.in);
+//		System.out.print("두개 정수 입력 : ");
+//		int n1 = sc.nextInt();
+//		int n2 = sc.nextInt();
+//		
+//		if(n1>=n2) {
+//			System.out.println("큰수 : " + n1);
+//		}else {
+//			System.out.println("큰수 : " + n2);
+//		}
+//
+//		sc.close();
+//		
+		//-----------------------
+		//3 문제(☆)
+		//-----------------------
 		// 세 개의 정수를 입력받아 큰 수 출력
+		Scanner sc = new Scanner(System.in);
+		System.out.print("세 개 정수 입력 : ");
+		int n1 = sc.nextInt();
+		int n2 = sc.nextInt();
+		int n3 = sc.nextInt();
+		
+		int max = n1;	//최대값(max)이 n1이라고 가정 시작 - !
+		
+		if(max<n2){
+			max=n2;
+		}
+		if(max<n3) {
+			max=n3;
+		}
 
-		//문제
+		System.out.println("큰 수 : " + max);
+		sc.close();
+		
+
+		//-----------------------
+		//4 문제
+		//-----------------------
 		// 세개의 정수를 입력받아 해당수의 합과 평균을 출력
 		
-		//문제
+		//-----------------------
+		//5 문제
+		//-----------------------
 //		입력한 수가 짝수이고, 3의 배수라면 출력
 //		입력한 수가 홀수이고, 5의 배수라면 출력
 	}
