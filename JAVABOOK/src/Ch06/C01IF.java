c186173b2ef1bca44bc0da64598e8bffeff41f21
junwoogyun <jwg8910@naver.com>
2025-02-27 10:28:45 +0900

    04 DAY

---
+++ JAVABOOK/src/Ch06/C01IF.java
@@ -0,0 +1,80 @@
+package Ch06;
+
+import java.util.Scanner;
+
+public class C01IF {
+
+	public static void main(String[] args) {
+		
+		//-----------------------
+		//단순 IF	
+		//-----------------------
+//		Scanner sc = new Scanner(System.in);
+//		System.out.print("나이 : " );
+//		int age = sc.nextInt();
+//		if(age >= 8)
+//			System.out.println("학교에 다닙니다.");
+//		
+//		System.out.println("첫번째 IF 코드 블럭 종료..");
+//		
+//		if(age<8)
+//			System.out.println("학교에 다니지 않습니다.");
+//		
+//		System.out.println("두번째 IF 코드 블럭 종료..");	
+//		System.out.println("프로그램을 종료합니다");
+//		
+//		sc.close();
+		
+		
+		//-----------------------
+		//IF-ELSE	
+		//-----------------------
+		
+//		Scanner sc = new Scanner(System.in);
+//		System.out.print("나이 : " );
+//		int age = sc.nextInt();
+//		
+//		if(age >= 8)
+//			System.out.println("학교에 다닙니다.");
+//		else
+//			System.out.println("학교에 다니지 않습니다.");
+//
+//		System.out.println("프로그램을 종료합니다");
+//		
+//		sc.close();
+		
+
+		//문제
+		// 정수 한개값을 입력받아 3의 배수이면 해당 수를 출력하세요
+		// 3의 배수이면서 5의 배수라면 출력 - 
+		// 순서도도 그려보세요 - 
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.print("정수입력 : " );
+		int num = sc.nextInt();
+		
+		if(num%3==0) {
+			System.out.printf("%d 는 3의 배수입니다\n",num);
+			
+			if(num%5==0) {
+				System.out.printf("%d 는 5의 배수입니다\n",num);
+			}
+		}
+		
+		sc.close();
+		
+		//문제
+		// 두개의 정수를 입력받아 큰 수 출력
+		
+		//문제(☆)
+		// 세 개의 정수를 입력받아 큰 수 출력
+
+		//문제
+		// 세개의 정수를 입력받아 해당수의 합과 평균을 출력
+		
+		//문제
+//		입력한 수가 짝수이고, 3의 배수라면 출력
+//		입력한 수가 홀수이고, 5의 배수라면 출력
+	}
+
+}
