442483894a2d63c2d910a05ee763afd67f8f0f17
junwoogyun <jwg8910@naver.com>
2025-02-28 14:49:33 +0900

    05 DAY

--- JAVABOOK/src/Ch07/C02While.java
+++ JAVABOOK/src/Ch07/C02While.java
@@ -305,24 +305,83 @@ public class C02While {
 //		i<4		j<4-i
 
 		
-		int i=0;
-		while(i<4) {
-			
-			int j=0;
-			while(j<4-i) {
-				System.out.print("*");
-				j++;
-			}
-			System.out.println();
-			i++;
-		}
+//		int i=0;
+//		while(i<4) {
+//			
+//			int j=0;
+//			while(j<4-i) {
+//				System.out.print("*");
+//				j++;
+//			}
+//			System.out.println();
+//			i++;
+//		}
+		
+
+//		i(개행)		j(별)
+//		0		0-(h-1) -0
+//		1		0-(h-1) -1
+//		2		0-(h-1) -2
+//		3		0-(h-1) -3
+//		...
+//		h-1		0-0
+//		-----------------------------
+//		i=0		j=0
+//		i++		j++
+//		i<h		j<h-i
+		
+		
+//		Scanner sc = new Scanner(System.in);
+//		int h = sc.nextInt();
+//		
+//		int i=0;
+//		while(i<h) {
+//			
+//			int j=0;
+//			while(j<h-i) {
+//				System.out.print("*");
+//				j++;
+//			}
+//			System.out.println();
+//			i++;
+//		}
+		
+		
 		
 		
 		//   *
 		//  ***
 		// *****
 		//*******
-	
+
+//		i(행)		j(공백)		k(별)
+//		0		0-2		0-0		
+//		1		0-1		0-2
+//		2		0-0		0-4
+//		3		x		0-6
+//		---------------------------------------
+//		i=0		j=0		k=0
+//		i++		j++		k++
+//		i<4		j<3-i		k<=2*i
+		
+		int i=0;
+		while(i<4) {
+			//공백
+			int j=0;
+			while(j<3-i) {
+				System.out.print(" ");
+				j++;
+			}
+			//별
+			int k=0;
+			while(k<=2*i) {
+				System.out.print("*");
+				k++;
+			}
+			System.out.println();
+			i++;
+		}
+		
 	
 		
 		//*******
