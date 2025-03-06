d333be8801a6ff96d6bf7eb7299d04226a3a9636
junwoogyun <jwg8910@naver.com>
2025-03-06 11:24:19 +0900

    08 DAY

--- JAVABOOK/src/Ch11/C02StaticMain.java
+++ JAVABOOK/src/Ch11/C02StaticMain.java
@@ -11,7 +11,8 @@ class C02Simple{
 	}
 	static void func2() {
 		n1=100;
-		n2=200;		// static 함수에서 사용되는 변수는, static변수 or 지역변수 선언이후 사용가능
+		//n2=200;		// static 함수에서 사용되는 변수는, static변수 or 지역변수 선언이후 사용가능
+						// 일반멤버변수로의 접근 불가능
 	}
 	
 }
