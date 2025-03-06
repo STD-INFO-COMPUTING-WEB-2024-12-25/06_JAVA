c180cbf60beb7a8b941996a5e9bcd66b4cb28758
junwoogyun <jwg8910@naver.com>
2025-03-06 11:26:14 +0900

    08 DAY

--- JAVABOOK/src/Ch11/C05Ex.java
+++ JAVABOOK/src/Ch11/C05Ex.java
@@ -1,21 +1,19 @@
 package Ch11;
-//다음 2개의 static 가진 ArrayUtils 클래스를 만들어보자. 다음 코드의 실행 결과를 참고하여 concat()와 print()를 작성하여 ArrayUtil 클래스를 완성하라.
-
-class ArrayUtils {
-	public static int[] concat(int[] a, int[] b) {
-		/* 배열 a와 b를 연결한 새로운 배열 리턴 */
-
-	}
-
-}
 
 public class C05Ex {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int[] array1 = { 1, 5, 7, 9 };
-		int[] array2 = { 3, 6, -1, 100, 77 };
-		int[] array3 = ArrayUtils.concat(array1, array2);
-		ArrayUtils.print(array3);
+		//---------------------------------------
+		//Practice
+		//---------------------------------------
+		//shopService 객체를 싱글톤으로 만들어 보세요 
+		//ShopServiceExample 클래스에서 shopService 의 getInstance() 메소드로 싱글톤을 얻을 수 있도록
+		//ShopService 클래스를 작성해보세요
+		ShopService obj1 = ShopService.getInstance();
+		ShopService obj2 = ShopService.getInstance();
+		if (obj1 == obj2) {
+			System.out.println("같은 ShopService 객체입니다.");
+		} else {
+			System.out.println("다른 ShopService 객체 입니다.");
+		}
 	}
 }
