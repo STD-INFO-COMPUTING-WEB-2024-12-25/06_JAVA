a52588c2d683129b4bf475a999f79a65aa5bfd67
eononenoe <jeonghyeon0921@naver.com>
2025-03-07 17:24:25 +0900

    no message

--- JAVABOOK/src/Ch13/Team_interface/삼조/Calculation.java
+++ JAVABOOK/src/Ch13/Team_interface/삼조/Calculation.java
@@ -6,23 +6,23 @@ public interface Calculation {
 	//해당 폴더에 있는 Div,Mul,Sub,Sum을 각각 역할 나눠서 함수를 완성하세요(C99Ex참고)
 	//나머지는 자유롭게 - !
 	void sum (int a,int b);
-	void sum (int a,int b,int c,int d,int e);
-	void sum (double a,double b,double c,double d);
-	void sum (String a,String b,String c,String d);
+	void sum (int ...args);
+	void sum (double ...args);
+	void sum (String ...args);
 	
 	void sub (int a,int b);
-	void sub (int a,int b,int c,int d,int e);
-	void sub (double a,double b,double c,double d);
-	void sub (String a,String b,String c,String d);
+	void sub (int ...args);
+	void sub (double ...args);
+	void sub (String ...args);
 	
 	void mul (int a,int b);
-	void mul (int a,int b,int c,int d,int e);
-	void mul (double a,double b,double c,double d);
-	void mul (String a,String b,String c,String d);
+	void mul (int ...args);
+	void mul (double ...args);
+	void mul (String ...args);
 	
 	void div (int a,int b);
-	void div (int a,int b,int c,int d,int e);
-	void div (double a,double b,double c,double d);
-	void div (String a,String b,String c,String d);
+	void div (int ...args);
+	void div (double ...args);
+	void div (String ...args);
 
 }
