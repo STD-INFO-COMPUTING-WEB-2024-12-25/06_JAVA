df02a9e9b1e21fd1d1d5041d846d37316232d388
Jaewoong-Hwang <boogiewoong2@gmail.com>
2025-03-07 17:10:42 +0900

    .

--- JAVABOOK/src/Ch13/Team_interface/일조/Calculation.java
+++ JAVABOOK/src/Ch13/Team_interface/일조/Calculation.java
@@ -6,5 +6,25 @@ public interface Calculation {
 	//해당 폴더에 있는 Div,Mul,Sub,Sum을 각각 역할 나눠서 함수를 완성하세요(C99Ex참고)
 	//나머지는 자유롭게 - !
 	
+	void sum (int a, int b);
+	void sum (int a, int b, int c, int d, int e);
+	void sum (double a, double b, double c, double d);
+	void sum (String a, String b, String c, String d);
+	
+	void sub (int a, int b);
+	void sub (int a, int b, int c, int d, int e);
+	void sub (double a, double b, double c, double d);
+	void sub (String a, String b, String c, String d);
+	
+	void mul (int a, int b);
+	void mul (int a, int b, int c, int d, int e);
+	void mul(double a, double b, double c, double d);
+	void mul (String a, String b, String c, String d);
+	
+	void div (int a, int b);
+	void div (int a, int b, int c, int d, int e);
+	void div(double a, double b, double c, double d);
+	void div (String a, String b, String c, String d);
+	
 
 }
