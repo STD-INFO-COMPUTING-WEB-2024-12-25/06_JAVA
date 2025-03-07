4e2af1c38990b0297dd5ac73850f2dbbfad9122f
eononenoe <jeonghyeon0921@naver.com>
2025-03-07 15:03:15 +0900

    3team

--- JAVABOOK/src/Ch14/Won2Yuan.java
+++ JAVABOOK/src/Ch14/Won2Yuan.java
@@ -1,5 +1,30 @@
 package Ch14;
 
-public class Won2Yuan {
+public class Won2Yuan extends Converter {
+	public void won2Yuan() {
+	}
+
+	public Won2Yuan(double ratio) {
+		super();
+		this.ratio = ratio;
+	}
+
+	@Override
+	protected double convert(double src) {
+		String c = Double.toString(src/ratio);
+		int d = c.indexOf(".")+2;
+		String e = c.substring(0, d);
+		return Double.parseDouble(e);
+	}
+
+	@Override
+	protected String getSrcString() {
+		return "원화";
+	}
+
+	@Override
+	protected String getDestString() {
+		return "위안";
+	}
 
 }
