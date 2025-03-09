825e239b1c9028e5dafb750d7fe51a374a30063a
Jaewoong-Hwang <boogiewoong2@gmail.com>
2025-03-09 17:20:10 +0900

    # interface Calculation update

--- JAVABOOK/src/Ch13/Team_interface/일조/Calculation.java
+++ JAVABOOK/src/Ch13/Team_interface/일조/Calculation.java
@@ -6,10 +6,23 @@ public interface Calculation {
 	// 해당 폴더에 있는 Div,Mul,Sub,Sum을 각각 역할 나눠서 함수를 완성하세요(C99Ex참고)
 	// 나머지는 자유롭게 - !
 
-	void Sum(int a, int b);
-	void Sum(int... args);
-	void Sum(int..... args);
-	void Sum(int....... args);
-	
+	void sum(int a, int b);
+	void sum(int... args);
+	void sum(double...args );
+	void sum(String... args);
 
+	void sub(int a, int b);
+	void sub(int... args);
+	void sub(double...args );
+	void sub(String... args);
+
+	void mul(int a, int b);
+	void mul(int... args);
+	void mul(double...args );
+	void mul(String... args);
+
+	void div(int a, int b);
+	void div(int... args);
+	void div(double...args );
+	void div(String... args);
 }
