b52933253cc4c1b1ac738c3a84e5ceafb8471ae9
junwoogyun <jwg8910@naver.com>
2025-03-10 14:32:00 +0900

    10 DAY

--- JAVABOOK/src/Ch14/C07LocalDateTimeFormatMain.java
+++ JAVABOOK/src/Ch14/C07LocalDateTimeFormatMain.java
@@ -1,6 +1,6 @@
 package Ch14;
 
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
@@ -9,24 +9,24 @@ public class C07LocalDateTimeFormatMain {
 	public static void main(String[] args) {
 
 		// 날짜정보 Fomatting
-//		Scanner sc = new Scanner(System.in);
-//        System.out.print("YYYY/MM/DD 입력 : ");
-//        String ymd = sc.next(); // 날짜 문자열 입력
-//
-//        // 입력용 포매터
-//        DateTimeFormatter inputFormatter = DateTimeFormatter.ofPattern("yyyy/MM/dd");
-//
-//        // 문자열을 LocalDate로 변환
-//        LocalDate date = LocalDate.parse(ymd, inputFormatter);
-//        System.out.println("입력된 날짜: " + date);
-//
-//        // 출력용 포매터
-//        DateTimeFormatter outputFormatter = DateTimeFormatter.ofPattern("yyyy~MM~dd");
-//
-//        // 포맷 변경 후 출력
-//        System.out.println("변환된 날짜: " + date.format(outputFormatter));
-//
-//        sc.close();
+		Scanner sc = new Scanner(System.in);
+        System.out.print("YYYY/MM/DD 입력 : ");
+        String ymd = sc.next(); // 날짜 문자열 입력
+
+        // 입력용 포매터
+        DateTimeFormatter inputFormatter = DateTimeFormatter.ofPattern("yyyy/MM/dd");
+
+        // 문자열을 LocalDate로 변환
+        LocalDate date = LocalDate.parse(ymd, inputFormatter);
+        System.out.println("입력된 날짜: " + date);
+
+        // 출력용 포매터
+        DateTimeFormatter outputFormatter = DateTimeFormatter.ofPattern("yyyy~MM~dd");
+
+        // 포맷 변경 후 출력
+        System.out.println("변환된 날짜: " + date.format(outputFormatter));
+
+        sc.close();
 
 		// 날짜시간 포매팅
 //		Scanner sc = new Scanner(System.in);
