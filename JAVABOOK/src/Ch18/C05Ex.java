bb09bdb7cfb0c600755a96e1be838fd1a63d3a80
junwoogyun <jwg8910@naver.com>
2025-03-11 16:38:23 +0900

    11 DAY

---
+++ JAVABOOK/src/Ch18/C05Ex.java
@@ -0,0 +1,19 @@
+package Ch18;
+
+public class C05Ex {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//Component 참조변수명
+		//textarea = area1
+		//textarea's scroll = scroll1
+		//파일로저장 : btn1
+		//1:1요청 : btn2
+		//대화기록보기 : btn3
+		//입력 : input
+		//textfield : txt1
+		
+	}
+
+}
