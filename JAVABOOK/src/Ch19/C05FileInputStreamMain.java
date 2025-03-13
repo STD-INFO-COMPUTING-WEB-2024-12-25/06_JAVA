945c81a73343a833079f5c25f7243cd84abc568a
jungwoogyun <jwg8910@naver.com>
2025-03-13 15:31:44 +0900

    14 DAY

--- JAVABOOK/src/Ch19/C05FileInputStreamMain.java
+++ JAVABOOK/src/Ch19/C05FileInputStreamMain.java
@@ -1,7 +1,25 @@
 package Ch19;
 
-public class C05FileInputStreamMain {
-	public static void main(String[] args) {
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
+public class C05FileInputStreamMain {
+	public static void main(String[] args) throws IOException {
+			
+		InputStream fin = new FileInputStream("C:\\IOTEST\\data.xlsx");
+		StringBuffer stringBuffer = new StringBuffer();
+		
+		//fin.read() 확인
+		while(true) {
+			int data = fin.read();
+			if(data==-1) break;
+			
+//			System.out.print((char)data);
+			
+		}
+		fin.close();
+		
+		
 	}
 }
