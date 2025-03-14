de8a6a4df87ba9be282043ca21881d04c3a2a644
jungwoogyun <jwg8910@naver.com>
2025-03-14 11:04:59 +0900

    15 DAY

---
+++ JAVABOOK/src/Ch19/C11SelenumAPIMain.java
@@ -0,0 +1,15 @@
+package Ch19;
+
+public class C11SelenumAPIMain {
+
+	public static void main(String[] args) {
+		//크롬서버.jar 다운로드 경로(셀레니움 공식 홈페이지)
+		//https://www.selenium.dev/downloads/
+		
+		//크롬드라이브 다운로드 경로
+		//https://googlechromelabs.github.io/chrome-for-testing/#stable
+		
+
+	}
+
+}
