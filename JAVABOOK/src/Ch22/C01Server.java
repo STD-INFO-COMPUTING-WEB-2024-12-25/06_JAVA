6aa92f5fb05fb148539cdf393d1f664c46cf2cae
jungwoogyun <jwg8910@naver.com>
2025-03-18 14:51:56 +0900

    17 DAY

--- JAVABOOK/src/Ch22/C01Server.java
+++ JAVABOOK/src/Ch22/C01Server.java
@@ -9,26 +9,26 @@ import java.net.Socket;
 public class C01Server {
 
 	public static void main(String[] args) throws IOException {
-		//서버 소켓 생성
-		ServerSocket server = new ServerSocket(7000); 	//192.168.16.203:7000
+		// 서버 소켓 생성
+		ServerSocket server = new ServerSocket(7000); // 192.168.16.203:7000
 		System.out.println("[INFO] SERVER SOCKET LISTENING");
-		
-		//
-		Socket client =  server.accept();
-		
-		//
-		OutputStream out =  client.getOutputStream();
-		DataOutputStream dout = new DataOutputStream(out);
-		
-		dout.writeUTF("[INFO] WELCOME TO SERVER");
-		
-		dout.flush();
-		dout.close();
-		out.close();
-		client.close();
-		server.close();
-		
-		System.out.println("서버 종료합니다");
+
+		while (true) {
+			//
+			Socket client = server.accept();
+			//
+			System.out.println("CLIENT IP : " + client.getInetAddress());
+			OutputStream out = client.getOutputStream();
+			DataOutputStream dout = new DataOutputStream(out);
+			dout.writeUTF("[INFO] WELCOME TO SERVER");
+			dout.flush();
+			dout.close();
+			out.close();
+			client.close();
+		}
+
+//		server.close();
+//		System.out.println("서버 종료합니다");
 	}
 
 }
