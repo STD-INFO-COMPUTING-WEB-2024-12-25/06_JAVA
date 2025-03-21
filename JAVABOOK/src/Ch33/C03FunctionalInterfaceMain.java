4dcc71a383393e3f3239bbcbfd93f0b45088be79
jungwoogyun <jwg8910@naver.com>
2025-03-21 12:21:49 +0900

    20 DAY

--- JAVABOOK/src/Ch33/C03FunctionalInterfaceMain.java
+++ JAVABOOK/src/Ch33/C03FunctionalInterfaceMain.java
@@ -1,5 +1,9 @@
 package Ch33;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+
 //제공되는 함수형 인터페이스들
 //Function<T, R>:
 //apply 메서드를 가지고 있어서 하나의 인자(T)를 받아서 결과(R)를 반환하는 함수형 인터페이스 입니다.
@@ -21,9 +25,40 @@ package Ch33;
 
 public class C03FunctionalInterfaceMain {
 
+	//01
+	public static Function<Integer, Integer> func1=x->x*x;
+	//02
+	public static Function<Integer, Integer> func2=x->x+x;
+	//03
+	public static Function< List<Integer>, Integer> func3=(x)->x.stream().reduce(0,(sum,el)->{return sum+el;});;
+	//04
+	public static Function< List<Object>, Integer> func4=(li)->{
+		//List<Object>로 받은 파라미터 중에 Integer만 추출해서 누적합 구하기
+		//1 Interger형만 필터링(instanceof)
+		//2 누적합 (reduce) 리턴
+		return -1;	
+	};
+	//05
+	public static Function< List<Object>, List<Integer> > func5=(li)->{
+		//List<Object>로 받은 파라미터 중에 Integer만 추출해서 리턴하기
+		//1 Interger형만 필터링(instanceof)
+		//2 재구성(map)
+		//3 리스트로 변환(collect)
+		return null;
+	};
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+		//01
+		System.out.println(func1.apply(10));
+		//02
+		System.out.println(func2.apply(10));
+		//03
+		System.out.println(func3.apply( Arrays.asList(10,20,30,40) ));
+		
+		
+		
 	}
+	
 
 }
